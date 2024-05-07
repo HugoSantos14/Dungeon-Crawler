ef03071f610cf6dd84226f0cd0bbc9f67eb25ae3
Hugo Santos <hugosantos14000@gmail.com>
2024-05-07 12:48:24 -0300

    Tutorial adicionado.

--- beta.c
+++ beta.c
@@ -4,12 +4,14 @@
 #include <conio.h>
 #include <string.h>
 #include <time.h>
+#include <ctype.h>
 
 int i, j;
 int player_i, player_j;
 int monster_i, monster_j;
 int hp = 30;
 int hp_max = 30;
+char input;
 char cheat[10];
 char fase1[15][15] = 
 
@@ -68,15 +70,17 @@ char fase2[30][30] =
 	{'*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*'}, //29
 };
 
-void linhaCol(int lin, int col){
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),(COORD){col - 1,lin - 1}); //coorddenada na tela
-    
-    //Cursor invisível
+void Cursor_Invisivel(){
     HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
     CONSOLE_CURSOR_INFO info;
     info.dwSize = 100;
     info.bVisible = FALSE;
     SetConsoleCursorInfo(consoleHandle, &info);
+}
+
+void linhaCol(int lin, int col){
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),(COORD){col - 1,lin - 1}); //coorddenada na tela
+    Cursor_Invisivel();
 }
 
 void mov_monstro1(){
@@ -142,6 +146,88 @@ void cheats(){
 	fflush(stdin);
 }
 
+void TutorialA(){
+	printf("Fase 1: \n\n\tDimensao: 15x15 \n\tElementos: \n\n\t1 - Jogador \n\t2 - Parede \n\t3 - Porta \n\t4 - Chave\n\n");
+	printf("Fase 2: \n\n\tDimensao: 30x30 \n\tElementos: \n\n\t1 - Todos os elementos da Fase 1 \n\t2 - Botao \n\t3 - Espinhos \n\t4 - Monstro Nivel 1\n\n");
+	printf("Fase 3: \n\n\tDimensao: 60x60 \n\tElementos: \n\n\t1 - Todos os elementos das Fases 1 e 2 \n\t2 - Teletransporte \n\t3 - Monstro Nivel 2\n\n");
+	printf("Aperte Z para voltar.\n");
+}
+
+void TutorialB(){
+	printf("Comandos:\n\n");
+    printf("\tW: Anda uma casa para cima.\n");
+    printf("\tA: Anda uma casa para a esquerda.\n");
+    printf("\tS: Anda uma casa para baixo.\n");
+    printf("\tD: Anda uma casa para a direita.\n");
+    printf("\tI: Interage com elementos do mapa.\n\n");
+	printf("Aperte Z para voltar.\n");
+}
+
+void TutorialC(){
+	printf("Elementos do jogo:\n\n");
+    printf("\t* - Parede \n\t& - jogador \n\tD - Porta fechada \n\t= - Porta aberta \n\t@ - Chave \n\tO - Botao \n\t# - Espinho \n\tX - Monstro nivel 1 \n\tV - Monstro nivel 2\n\n");
+	printf("Aperte Z para voltar.\n");
+}
+
+void GerarTutorial(){
+    Cursor_Invisivel();
+    system("cls");
+	printf("Qual conhecimento deseja buscar?\n\n\tA - Fases\n\tB - Comandos\n\tC - Elementos do jogo\n\tZ - Sair\n");
+    input = getch();
+	system("cls");
+    switch(toupper(input)){
+        case 'A':
+            TutorialA();
+		    input = getch();
+            switch (toupper(input)){
+                case 'Z':
+		            system("cls");
+                    GerarTutorial();
+            }
+            break;
+            
+        case 'B':
+            TutorialB();
+		    input = getch();
+            switch (toupper(input)){
+                case 'Z':
+		            system("cls");
+                    GerarTutorial();
+            }
+            break;
+            
+        case 'C':
+            TutorialC();
+		    input = getch();
+            switch (toupper(input)){
+                case 'Z':
+		            system("cls");
+                    GerarTutorial();
+            }
+            break;
+                
+        case 'Z':
+			printf("\nSaindo");
+    		for (i = 0; i < 5; i++){
+        	Sleep(200);
+        	printf(".");
+    		}
+            break;
+    }
+}
+
+void Tutorial(){
+    system("cls");
+	printf("\nBem-vindo ao tutorial!\n\n\tCarregando");
+    for (i = 0; i < 5; i++){
+        Sleep(200);
+        printf(".");
+    }
+    system("cls");
+    GerarTutorial();
+	PrimeiraFase();
+}
+
 void GerarFase1(char fase1[15][15]){
 	printf("\n\n");
     for (i = 0; i < 15; i++){
@@ -174,11 +260,9 @@ void GerarFase2(char fase2[30][30]){
     }
 }
 
-
 void PrimeiraFase(){
 	player_i = 1;
 	player_j = 1;
-	char input;
 	linhaCol(1, 1);
 	
 	while(1){
@@ -186,24 +270,21 @@ void PrimeiraFase(){
 		GerarFase1(fase1);
 		printf("\n\tHP: %d/%d\n", hp, hp_max);
 		input = getch();
-		switch (input){
+		switch (toupper(input)){
 			case 'W':
-    		case 'w':
             	if (fase1[player_i - 1][player_j] != '*'){
                 	player_i--;
             	}
             	break;
             
             case 'A':
-        	case 'a':
         		
 				if ((fase1[player_i][player_j - 1] == '@') && (player_i != 7)){
 					printf("\n\nAperte I para pegar a chave.\n");
 					input = getch();
-					switch (input){
+					switch (toupper(input)){
 						case 'I':
-						case 'i':
-							printf("Nao aconteceu nada... parece que a chave era falsa.\n\n");
+							printf("\nNao aconteceu nada... parece que a chave era falsa.\n\n");
 							system("pause");
 						
 						default:
@@ -222,9 +303,8 @@ void PrimeiraFase(){
 				if ((fase1[7][1] == '@') && (player_i == 7) && (player_j - 1 == 0)){
 					printf("\n\nAperte I para pegar a chave.\n");
 					input = getch();
-					switch (input){
+					switch (toupper(input)){
 						case 'I':
-						case 'i':
 							fase1[7][1] = ' ';
 							fase1[9][12] = ' ';
 							fase1[12][14] = '=';
@@ -241,14 +321,12 @@ void PrimeiraFase(){
             	break;
             
             case 'S':
-        	case 's':
 				
 				if (fase1[player_i + 1][player_j] == '@'){
 					printf("\n\nAperte I para pegar a chave.\n");
 					input = getch();
-					switch (input){
+					switch (toupper(input)){
 						case 'I':
-						case 'i':
 							printf("Nao aconteceu nada... parece que a chave era falsa.\n\n");
 							system("pause");
 						
@@ -262,13 +340,11 @@ void PrimeiraFase(){
             	break;
             
             case 'D':
-        	case 'd':
 				if (fase1[player_i][player_j + 1] == '@'){
 					printf("\n\nAperte I para pegar a chave.\n");
 					input = getch();
-					switch (input){
+					switch (toupper(input)){
 						case 'I':
-						case 'i':
 							printf("Nao aconteceu nada... parece que a chave era falsa.\n\n");
 							system("pause");
 						
@@ -299,8 +375,6 @@ void SegundaFase(){
 	monster_i = 7;
 	monster_j = 14;
 	fase2[monster_i][monster_j] = 'X';
-
-	char input;
 	linhaCol(27, 1);
 
 	while(1){
@@ -308,29 +382,29 @@ void SegundaFase(){
 		GerarFase2(fase2);
 		printf("\n\tHP: %d/%d\n", hp, hp_max);
 		input = getch();
-		switch(input){
-			case 'w':
+		switch(toupper(input)){
+			case 'W':
 				if (fase2[player_i - 1][player_j] != '*'){
                 	player_i--;
 					mov_monstro1();
             	}
             	break;
 			
-			case 'a':
+			case 'A':
 				if ((fase2[player_i][player_j - 1] != '*')){
                 	player_j--;
 					mov_monstro1();
             	}
 				break;
 
-			case 's':
+			case 'S':
 				if (fase2[player_i + 1][player_j] != '*'){
                 	player_i++;
 					mov_monstro1();
             	}
 
             	break;
-			case 'd':
+			case 'D':
 				if ((fase2[player_i][player_j + 1] != '*')){
                 	player_j++;
 					mov_monstro1();
@@ -343,7 +417,8 @@ void SegundaFase(){
 
 int main(){
 	
-	PrimeiraFase();
+	Tutorial();
+	//PrimeiraFase();
 	//SegundaFase();
 	//cheats();
 	
